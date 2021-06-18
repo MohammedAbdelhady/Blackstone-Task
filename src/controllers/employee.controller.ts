1d382260f005f7f8fc34ad6a020c4bb23e32d892
Gomaa <gomaa@garment.io>
2021-06-18 18:28:58 +0200

    remove un relevant employee routes

--- src/controllers/employee.controller.ts
+++ src/controllers/employee.controller.ts
@@ -1,21 +1,11 @@
 import {
-  Count,
-  CountSchema,
   Filter,
-  FilterExcludingWhere,
-  repository,
-  Where,
+  repository
 } from '@loopback/repository';
 import {
-  post,
-  param,
   get,
-  getModelSchemaRef,
-  patch,
-  put,
-  del,
-  requestBody,
-  response,
+  getModelSchemaRef, param, post, requestBody,
+  response
 } from '@loopback/rest';
 import {Employee} from '../models';
 import {EmployeeRepository} from '../repositories';
@@ -47,17 +37,6 @@ export class EmployeeController {
     return this.employeeRepository.create(employee);
   }
 
-  @get('/employees/count')
-  @response(200, {
-    description: 'Employee model count',
-    content: {'application/json': {schema: CountSchema}},
-  })
-  async count(
-    @param.where(Employee) where?: Where<Employee>,
-  ): Promise<Count> {
-    return this.employeeRepository.count(where);
-  }
-
   @get('/employees')
   @response(200, {
     description: 'Array of Employee model instances',
@@ -76,75 +55,4 @@ export class EmployeeController {
     return this.employeeRepository.find(filter);
   }
 
-  @patch('/employees')
-  @response(200, {
-    description: 'Employee PATCH success count',
-    content: {'application/json': {schema: CountSchema}},
-  })
-  async updateAll(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Employee, {partial: true}),
-        },
-      },
-    })
-    employee: Employee,
-    @param.where(Employee) where?: Where<Employee>,
-  ): Promise<Count> {
-    return this.employeeRepository.updateAll(employee, where);
-  }
-
-  @get('/employees/{id}')
-  @response(200, {
-    description: 'Employee model instance',
-    content: {
-      'application/json': {
-        schema: getModelSchemaRef(Employee, {includeRelations: true}),
-      },
-    },
-  })
-  async findById(
-    @param.path.string('id') id: string,
-    @param.filter(Employee, {exclude: 'where'}) filter?: FilterExcludingWhere<Employee>
-  ): Promise<Employee> {
-    return this.employeeRepository.findById(id, filter);
-  }
-
-  @patch('/employees/{id}')
-  @response(204, {
-    description: 'Employee PATCH success',
-  })
-  async updateById(
-    @param.path.string('id') id: string,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Employee, {partial: true}),
-        },
-      },
-    })
-    employee: Employee,
-  ): Promise<void> {
-    await this.employeeRepository.updateById(id, employee);
-  }
-
-  @put('/employees/{id}')
-  @response(204, {
-    description: 'Employee PUT success',
-  })
-  async replaceById(
-    @param.path.string('id') id: string,
-    @requestBody() employee: Employee,
-  ): Promise<void> {
-    await this.employeeRepository.replaceById(id, employee);
-  }
-
-  @del('/employees/{id}')
-  @response(204, {
-    description: 'Employee DELETE success',
-  })
-  async deleteById(@param.path.string('id') id: string): Promise<void> {
-    await this.employeeRepository.deleteById(id);
-  }
 }
