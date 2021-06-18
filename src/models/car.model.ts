bbce36673f1137e4a3bda6d59f19caca343f67b8
Gomaa <gomaa@garment.io>
2021-06-18 16:14:07 +0200

    adding car model

---
+++ src/models/car.model.ts
@@ -0,0 +1,40 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Car extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  brand: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  model: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  plateNumber: string;
+
+
+  constructor(data?: Partial<Car>) {
+    super(data);
+  }
+}
+
+export interface CarRelations {
+  // describe navigational properties here
+}
+
+export type CarWithRelations = Car & CarRelations;
