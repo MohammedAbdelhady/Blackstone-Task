cdd704cbff98042ed2700f8076d6198fffc8b029
Gomaa <gomaa@garment.io>
2021-06-18 16:13:54 +0200

    adding employee model

---
+++ src/models/employee.model.ts
@@ -0,0 +1,40 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Employee extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  position: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  age: number;
+
+
+  constructor(data?: Partial<Employee>) {
+    super(data);
+  }
+}
+
+export interface EmployeeRelations {
+  // describe navigational properties here
+}
+
+export type EmployeeWithRelations = Employee & EmployeeRelations;
